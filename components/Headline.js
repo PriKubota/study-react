b691cb115ace1168e6c28fa72ab473f46fd1ddd9
kubota sora <sora8han@gmail.com>
2022-03-27 09:11:35 +0900

    :art: props第二弾

--- components/Headline.js
+++ components/Headline.js
@@ -3,17 +3,14 @@ import Image from 'next/image'
 import React from 'react';
 import styles from '../styles/Home.module.css'
 
-export default function Headline(props) {
-    console.log(props.title);
-
+export function Headline(props) {
     return (
     <div>
         <h1 className={styles.title}>
         {props.page}Page!!kubota
         </h1>
         <p className={styles.description}>
-        Get started by editing{' '}
-        <code className={styles.code}>pages/{props.page}.js</code>
+        Get started by editing {props.children}
         </p>
     </div>
   )
