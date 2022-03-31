7c1cc6968a90f0c254530941799bd360c281a7bc
kubota sora <sora8han@gmail.com>
2022-03-31 20:52:07 +0900

    :wheelchair: useCallback

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,33 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
+import { useCallback } from 'react'
 
 export default function Home() {
+
+  const foo = 1;
+
+  // コンポネント内に書くパターンは再レンダリング時に再生成されるデメリットがある
+  // コンポネントの外に書くと引数を渡すことが必要になったりして煩雑になる可能性もある
+  // useCallbackで再レンダリング時に生成されないのでパフォーマンスが向上する
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>index page</title>
       </Head>
       <Header />
+      <a href='/about'
+        onClick={handleClick}
+        /* onClick={() => alert(123)} */
+        >ボタン</a>
       <Main page="index" />
       <Footer />
     </div>
