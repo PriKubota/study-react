d76089c5f761674a1e878e385b3d6c5bb352a1a3
kubota sora <sora8han@gmail.com>
2022-04-01 12:07:03 +0900

    state追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,20 +3,23 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from '../components/Header'
-import { useCallback, useEffect } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function Home() {
 
-  // const foo = 1;
+  const [count , setCount] = useState(1);
+  // let foo = 1;
+
 
   // コンポネント内に書くパターンは再レンダリング時に再生成されるデメリットがある
   // コンポネントの外に書くと引数を渡すことが必要になったりして煩雑になる可能性もある
   // useCallbackで再レンダリング時に生成されないのでパフォーマンスが向上する
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+    // foo += 1;
+    // console.log(foo)
+    // 前の状態を用いてそれに対して処理を行いたい場合は関数で記載する
+    setCount((count) => count + 1);
+  };
 
   useEffect(() => {
     // マウント時の処理
@@ -31,16 +34,18 @@ export default function Home() {
     }
   }, []);
 
+
   return (
     <div className={styles.container}>
       <Head>
         <title>index page</title>
       </Head>
       <Header />
-      {/* <a href='/about'
+      <h1>{count}</h1>
+      <button href='/about'
         onClick={handleClick}
-         onClick={() => alert(123)} 
-        >ボタン</a> */}
+        //  onClick={() => alert(123)} 
+        >ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
